ec7cfe8681d7eca6fcfa94791e943f225ba1c382
fishg7758-hub <fishg7758@gmail.com>
2026-09-14 20:40:04 +0500

    Update server.js

--- server.js
+++ server.js
@@ -148,6 +148,9 @@ app.get("/api/quote", async (req, res) => {
 app.get("/", (req, res) => {
   res.sendFile(path.join(process.cwd(), "public", "index.html"));
 });
+
+app.listen(port, () => console.log(`MUBEE AI running at http://localhost:${port}`));
+});
 });
 
 app.listen(port, () => console.log(`MUBEE AI running at http://localhost:${port}`));
